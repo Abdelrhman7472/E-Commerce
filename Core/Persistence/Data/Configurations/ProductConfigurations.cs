91d993daad3c379e781f638e611fdfe85483a9c7
Abdelrhman7472 <117462551+Abdelrhman7472@users.noreply.github.com>
2024-09-30 03:43:42 +0300

    Add project files.

---
+++ Core/Persistence/Data/Configurations/ProductConfigurations.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence.Data.Configurations
+{
+    internal class ProductConfigurations : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasOne(product=>product.ProductBrand).WithMany().HasForeignKey(product=>product.BrandId);
+
+            builder.HasOne(product=>product.ProductType).WithMany().HasForeignKey(product=>product.TypeId);
+
+            builder.Property(p => p.Price).HasColumnType("decimal(18,3)");
+
+        }
+    }
+}
